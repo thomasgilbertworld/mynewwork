db03b658b3a58aa701f4a6f75b35872f62848c4d
thomasgilbertworld <tgirbert37@gmail.com>
2024-05-12 17:26:38 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/ebd551f671.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="index.css">
+    <title>Document</title>
+</head>
+<body>
+
+ <div class="in">
+  <div class="jw">
+  <i class="fa-solid fa-phone r"></i>
+  <p> +0805744182</p>
+  </div>
+  <div class="wre">
+     <i class="fa-solid fa-paper-plane r"></i>
+    <p>YOUREMAIL@EMAIL.COM</p>
+     </div>
+    <div>
+    <p>3-5 BUSINESS DAYS DELIVERY & FREE RETURNS</p>
+  </div>
+ </div>
+
+<div class="wtr">
+
+  <div>
+  <p class="fruit">VEGEFOODS</p>
+  </div>
+ <div class="dis">
+  <p class="weq"><a href="">Home</a></p>
+  <p class="weq"><a href="">Shop</a> <i class="fa-solid fa-caret-down"></i></p>
+  <p class="weq"><a href="">about</a></p>
+  <p class="weq"><a href="">blog</a></p>
+  <p class="weq"><a href="">Contact</a></p>
+  <p class="weq"><a href=""><i class="fa-solid fa-cart-shopping"></i>[0]</a></p>
+ </div>
+
+</div>
+<div class="im">
+  <div class="c">
+  <p class="re">100% FRESH & ORGANIC FOOD</p>
+  <p class="thes">WE DELIVE ORGANIC VEGETABLE & FRUITS </p>
+  <p class="gre"><a href="">View Details</a></p>
+  </div>
+
+</body>
+</html>
