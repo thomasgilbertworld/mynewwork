69a8791582495b67bbad7bfae3ed11432ad8dbd1
thomasgilbertworld <tgirbert37@gmail.com>
2024-06-23 16:20:47 +0100

    d

---
+++ index.js
@@ -0,0 +1,12 @@
+let future=new Date("2024-6-19 20:00:00");
+
+let current=new Date();
+console.log()
+
+document.querySelector(".pop").innerHTML=future.getDate()-current.getDate();
+document.querySelector(".fd").innerHTML=future.getDate()-current.getHours();
+future.getHours()
+
+let menu=document.querySelector('.l')
+
+    
--- index.php
+++ index.php
@@ -6,35 +6,41 @@
     <script src="https://kit.fontawesome.com/ebd551f671.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="index.css">
     <title>Document</title>
+    <script src="index.js" defer></script>
 </head>
 <body>
 
- <div class="in">
+<div class="in">
   <div class="jw">
   <i class="fa-solid fa-phone r"></i>
   <p> +0805744182</p>
   </div>
   <div class="wre">
      <i class="fa-solid fa-paper-plane r"></i>
-    <p>YOUREMAIL@EMAIL.COM</p>
-     </div>
-    <div>
-    <p>3-5 BUSINESS DAYS DELIVERY & FREE RETURNS</p>
+     <p>YOUREMAIL@EMAIL.COM</p>
+  </div>
+  <div class="wre">
+     <p>3-5 BUSINESS DAYS DELIVERY & FREE RETURNS</p>
   </div>
  </div>
 
 <div class="wtr">
-
+ <div class="gilbert"> 
   <div>
   <p class="fruit">VEGEFOODS</p>
+  
   </div>
+  <div>
+  <p class="l"><i class="fa-solid fa-bars"></i> MENU</p>
+  </div>
+ </div>
  <div class="dis">
-  <p class="weq"><a href="">Home</a></p>
+  <p class="weqs"><a href="">Home</a></p>
   <p class="weq"><a href="">Shop</a> <i class="fa-solid fa-caret-down"></i></p>
   <p class="weq"><a href="">about</a></p>
   <p class="weq"><a href="">blog</a></p>
   <p class="weq"><a href="">Contact</a></p>
-  <p class="weq"><a href=""><i class="fa-solid fa-cart-shopping"></i>[0]</a></p>
+  <p class="weqe"><a href=""><i class="fa-solid fa-cart-shopping"></i>[0]</a></p>
  </div>
 
 </div>
@@ -44,6 +50,227 @@
   <p class="thes">WE DELIVE ORGANIC VEGETABLE & FRUITS </p>
   <p class="gre"><a href="">View Details</a></p>
   </div>
+</div>
+<div class="lp">
+ <div class="inters ">
+  <p class="inter"><i class="fa-solid fa-truck cd"></i></p>
+  <p class="wes k">FREE SHOPPING</p>
+  <p class="free v">ON ORDER OVER $100</p>
+  </div>
+  <div class="inters">
+  <p class="intee"><i class="fa-solid fa-carrot cd"></i></i></p>
+  <p class="wes">ALWAYS FRESH</p>
+  <p class="free q">PRODUCT WELL PACKAGE</p>
+  </div>
+  <div class="inters">
+  <p class="interq"><i class="fa-solid fa-award cd"></i></p>
+  <p class="wes ty">SUPERIOR QUALITY</p>
+  <p class="free hj">QUALITY PRODUCTS</p>
+  </div>
+  <div class="inters ">
+  <p class="intera"><i class="fa-solid fa-id-card cd"></i> </p>
+  <p class="wes w">SUPPORT</p>
+  <p class="free d ">24/7 SUPPORT</p>
+  </div>
+</div>
+<div class="grind">
+<div class='foels'>
+  <p class="vegi">Vegitable</p>
+  <p class="psting">Protect the health of every home</p>
+  <p class="with"><a href="">shop now</a></p>
+ </div>
+ <div>
+   <img src="img//inq.jpg" alt="" class="eta">
+   <p class="day">Fruit</p>
+ </div>
+ <div class='foel'>
+  <p class="vegi">Vegitable</p>
+  <p class="psting">Protect the health of every home</p>
+  <p class="with"><a href="">shop now</a></p>
+ </div>
+ <div>
+   <img src="img//inq.jpg" alt="" class="eta">
+   <p class="day">vegitable</p>
+ </div>
+ 
+ 
+ <div>
+   <img src="img//download2.jpg" alt="" class="eta">
+   <p class="day">juices</p>
+ </div>
+ <div>
+  <img src="img//myself.jpg" alt="" class="swq">
+ </div>
+ <div>
+   <img src="img//imagesa.jpg" alt="" class="eta">
+   <p class="day">dried</p>
+ </div>
+ 
+</div> 
+<div class="nters">
+<p class="nter">Featured Products</p>
+<p class="your">Our Products</p>
+<p class="cas">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia</p>
+</div>
 
+
+
+<div class="pol">
+
+
+ <div class="fg">
+  <p class="dayw">30%</p>
+  <img src="img/gilbertthomas.jpg" alt="" srcset=""  class="ina">
+  <p class="mak">BELL PEPPER</p>
+  <p class="was"><strike class="sq">$120.00</strike> $80.00</p>
+  <div class="mt">
+    <p class="lo"><a href=""><i class="fa-solid fa-bars"></i></a></p>
+    <p class="lo"><a href=""><i class="fa-solid fa-cart-shopping"></i></a></p>
+    <p class="lo"><a href=""><i class="fa-solid fa-heart"></i></a></p>
+  </div>
+ </div>
+ <div class="fg">
+  <p class="dayw">30%</p>
+  <img src="img/myimg.jpg" alt="" srcset=""  class="ina">
+  <p class="mak">Tomato</p>
+  <p class="was"><strike class="sq">$120.00</strike> $80.00</p>
+  <div class="mt">
+    <p class="lo"><a href=""><i class="fa-solid fa-bars"></i></a></p>
+    <p class="lo"><a href=""><i class="fa-solid fa-cart-shopping"></i></a></p>
+    <p class="lo"><a href=""><i class="fa-solid fa-heart"></i></a></p>
+  </div>
+ </div>
+ <div class="fg">
+
+  <img src="img/fe.jpg" alt="" srcset=""  class="ina">
+  <p class="mak">strawberry</p>
+  <p class="was">$120.00 </p>
+  <div class="mt">
+    <p class="lo"><a href=""><i class="fa-solid fa-bars"></i></a></p>
+    <p class="lo"><a href=""><i class="fa-solid fa-cart-shopping"></i></a></p>
+    <p class="lo"><a href=""><i class="fa-solid fa-heart"></i></a></p>
+  </div>
+ </div>
+ <div class="fg">
+
+  <img src="img/im.jpg" alt="" srcset=""  class="ina">
+  <p class="mak">broccil</p>
+  <p class="was">$120.00</p>
+  <div class="mt">
+    <p class="lo"><a href=""><i class="fa-solid fa-bars"></i></a></p>
+    <p class="lo"><a href=""><i class="fa-solid fa-cart-shopping"></i></a></p>
+    <p class="lo"><a href=""><i class="fa-solid fa-heart"></i></a></p>
+  </div>
+ </div>
+ <div class="fg">
+
+  <img src="img/s.jpg" alt="" srcset=""  class="ina">
+  <p class="mak">GREEN BEAN</p>
+  <p class="was">$120.00</p>
+  <div class="mt">
+    <p class="lo"><a href=""><i class="fa-solid fa-bars"></i></a></p>
+    <p class="lo"><a href=""><i class="fa-solid fa-cart-shopping"></i></a></p>
+    <p class="lo"><a href=""><i class="fa-solid fa-heart"></i></a></p>
+  </div>
+ </div>
+
+ <div class="fg">
+
+  <img src="img/yoursq.jpg" alt="" srcset=""  class="ina">
+  <p class="mak">purple cabbage </p>
+  <p class="was">$120.00</p>
+  <div class="mt">
+    <p class="lo"><a href=""><i class="fa-solid fa-bars"></i></a></p>
+    <p class="lo"><a href=""><i class="fa-solid fa-cart-shopping"></i></a></p>
+    <p class="lo"><a href=""><i class="fa-solid fa-heart"></i></a></p>
+  </div>
+ </div>
+
+ <div class="fg">
+
+  <img src="img/sa.jpg" alt="" srcset=""  class="ina">
+  <p class="mak">carrot</p>
+  <p class="was">$120.00</p>
+  <div class="mt">
+    <p class="lo"><a href=""><i class="fa-solid fa-bars"></i></a></p>
+    <p class="lo"><a href=""><i class="fa-solid fa-cart-shopping"></i></a></p>
+    <p class="lo"><a href=""><i class="fa-solid fa-heart"></i></a></p>
+  </div>
+ </div>
+ <div class="fg">
+
+  <img src="img/q.jpg" alt="" srcset=""  class="ina">
+  <p class="mak">furit jusices </p>
+  <p class="was">$120.00</p>
+  <div class="mt">
+    <p class="lo"><a href=""><i class="fa-solid fa-bars"></i></a></p>
+    <p class="lo"><a href=""><i class="fa-solid fa-cart-shopping"></i></a></p>
+    <p class="lo"><a href=""><i class="fa-solid fa-heart"></i></a></p>
+  </div>
+ </div>
+
+
+</div>
+
+
+
+
+<div class="my">
+ <div class="wy">
+  <p class="homa">Best Price For You</p>
+  <p class="q2">Deal of the day</p>
+  <p class="t2">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia</p>
+  <a href="" class="heat">Spinach</a>
+  <p class="d2">$10 <span class="f2">now $5 only</span></p>
+
+  <div class="gred">
+  <p class="school"><span class="pop">0</span><span class="fors">Days</span> </p>
+   <p class="school"><span class="fd">0</span><span class="fors">hours</span> </p>
+   <p class="school">22 <span class="fors">minuts</span> </p>
+   <p class="school">10<span class="fors">seconds</span> </p>
+  </div>
+ </div>
+</div>
+
+<div class="wt">
+ <img src="newimg/for.jpg" alt="" class="o">
+ <img src="newimg/q.jpg" alt=""  class="o">
+ <img src="newimg/dice.jpg" alt="" class="o">
+ <img src="newimg/copy.jpg" alt="" class="o">
+ <img src="newimg/anog.jpg" alt="" class="o">
+</div>
+
+
+
+
+<div class="work">
+  <div class="d">
+    <p class="jojo">Vegefoods</p>
+    <p class="dio">Far far away, behind the word <br> mountains, far from the <br>countries Vokalia and <br> Consonantia.</p>
+    <i class="fa-brands fa-twitter s"></i>
+    <i class="fa-brands fa-facebook-f s"></i>
+    <i class="fa-brands fa-instagram s"></i>
+  </div>
+  <div class="d">
+    <p>menu</p>
+    <a href="" class="sa">shop</a>
+    <a href="" class="sa">about</a>
+    <a href="" class="sa">journal</a>
+    <a href="" class="sa">contact us</a>
+  </div>
+  <div class="d">
+    <p>help</p>
+    <a href="" class="sa">shopping FAQs</a>
+    <a href="" class="sa">Returns & Exchange</a>
+    <a href="" class="sa">Terms & Conditions</a>
+    <a href="" class="sa">Privacy Policy</a>
+  </div>
+  <div class="d">
+    <p>Have a Question?</p>
+    <p class="ce"><i class="fa-solid fa-location-dot "></i> 203 Fake St. Mountain <br> View, San Francisco, <br> California, USA</p>
+    <p class="ce"><i class="fa-solid fa-phone"></i>+ 08057444182</p>
+    <p class="ce"><i class="fa-solid fa-envelope"> </i>	info@yourdomain.com</p>
+  </div>
+</div>
 </body>
 </html>
